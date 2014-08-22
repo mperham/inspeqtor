b40b89535a118488a157304ebf99d0acd796ba87
Mike Perham <mperham@gmail.com>
2014-08-22 12:13:31 -0700

    Rejigger flag parsing

--- cli/cli.go
+++ cli/cli.go
@@ -12,7 +12,16 @@ type CmdOptions struct {
 	TestNotifications bool
 	ConfigDirectory   string
 	LogLevel          string
+	SocketPath        string
 }
+
+var (
+	SalesPitch = func() {
+		log.Println("Want more? Upgrade to Inspeqtor Pro for more features and official support.")
+		log.Println("See http://contribsys.com/inspeqtor for details.")
+		log.Println("")
+	}
+)
 
 func SetupLogging() {
 	// Modern POSIX processes should log to STDOUT only
@@ -24,36 +33,41 @@ func SetupLogging() {
 }
 
 func ParseArguments(name string, version string) CmdOptions {
-	defaults := CmdOptions{false, false, "/etc/inspeqtor", "info"}
-
-	flag.BoolVar(&defaults.TestConfig, "t", false, "Verify configuration and exit")
-	flag.BoolVar(&defaults.TestNotifications, "tn", false, "Verify notifications and exit")
-	flag.StringVar(&defaults.ConfigDirectory, "c", "/etc/inspeqtor", "Configuration directory")
-	flag.StringVar(&defaults.LogLevel, "l", "info", "Logging level (warn, info, debug, verbose)")
-	helpPtr := flag.Bool("help", false, "You're looking at it")
-	help2Ptr := flag.Bool("h", false, "You're looking at it")
-	flag.Parse()
+	defaults := CmdOptions{false, false, "/etc/inspeqtor", "info", "/var/run/inspeqtor.sock"}
 
 	log.Println(name, version)
 	log.Println("Copyright © 2014 Contributed Systems LLC")
 	log.Println("Licensed under the GNU Public License Version 3")
 	log.Println("")
-	log.Println("Want more? Upgrade to Inspeqtor Pro for more features and official support.")
-	log.Println("See http://contribsys.com/inspeqtor for details.")
-	log.Println("")
+
+	if SalesPitch != nil {
+		SalesPitch()
+	}
+
+	flag.Usage = help
+	flag.BoolVar(&defaults.TestConfig, "t", false, "Verify configuration and exit")
+	flag.BoolVar(&defaults.TestNotifications, "tn", false, "Verify notifications and exit")
+	flag.StringVar(&defaults.LogLevel, "l", "info", "Logging level (warn, info, debug, verbose)")
+
+	// undocumented on purpose, for testing only
+	flag.StringVar(&defaults.SocketPath, "s", "/var/run/inspeqtor.sock", "")
+	flag.StringVar(&defaults.ConfigDirectory, "c", "/etc/inspeqtor", "")
+	helpPtr := flag.Bool("help", false, "You're looking at it")
+	help2Ptr := flag.Bool("h", false, "You're looking at it")
+	flag.Parse()
 
 	if *helpPtr || *help2Ptr {
-		log.Println("")
-		log.Println("-c [dir]\tConfiguration directory, default: /etc/inspeqtor")
-		log.Println("-l [level]\tSet logging level (warn, info, debug, verbose), default: info")
-		log.Println("-t\t\tVerify configuration and exit")
-		log.Println("-tn\t\tVerify notifications and exit")
-		log.Println("")
-		log.Println("-h\t\tYou're looking at it")
+		help()
 		os.Exit(0)
 	}
 
 	util.SetLogLevel(defaults.LogLevel)
 
 	return defaults
+}
+
+func help() {
+	log.Println("-l [level]\tSet logging level (warn, info, debug, verbose), default: info")
+	log.Println("-t\t\tVerify configuration and exit")
+	log.Println("-tn\t\tVerify notifications and exit")
 }
