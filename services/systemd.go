4e8be8bbcae1320edc6bfa16da6f29501f1d5669
Mike Perham <mperham@gmail.com>
2014-09-08 14:01:35 -0700

    Initial systemd support

---
+++ services/systemd.go
@@ -0,0 +1,103 @@
+package services
+
+import (
+	"errors"
+	"inspeqtor/util"
+	"os/exec"
+	"path/filepath"
+	"strconv"
+	"strings"
+)
+
+type Systemd struct {
+	path         string
+	dummyOutput  string
+	dummyOutput2 string
+}
+
+func detectSystemd(path string) (InitSystem, error) {
+	result, err := util.FileExists(path)
+	if err != nil {
+		return nil, err
+	}
+
+	if !result {
+		util.Debug("systemd not detected, no " + path)
+		return nil, nil
+	}
+
+	matches, err := filepath.Glob(path + "/*.conf")
+	if err != nil {
+		return nil, err
+	}
+
+	if len(matches) > 0 {
+		util.Info("Detected systemd in " + path)
+		return &Systemd{path, "", ""}, nil
+	}
+
+	util.Debug("systemd not detected, empty " + path)
+	return nil, nil
+}
+
+func (u *Systemd) Name() string {
+	return "systemd"
+}
+
+func (u *Systemd) Restart(serviceName string) error {
+	if len(u.dummyOutput) != 0 {
+		//sout = []byte(u.dummyOutput)
+	} else {
+		cmd := exec.Command("systemctl", "restart", serviceName)
+		_, err := cmd.CombinedOutput()
+		if err != nil {
+			return &ServiceError{u.Name(), serviceName, err}
+		}
+	}
+
+	return nil
+}
+
+func (u *Systemd) LookupService(serviceName string) (*ProcessStatus, error) {
+	var sout []byte
+	var err error
+
+	if len(u.dummyOutput) != 0 {
+		sout = []byte(u.dummyOutput)
+	} else {
+		cmd := exec.Command("systemctl", "show", "-p", "MainPID", serviceName)
+		sout, err = cmd.CombinedOutput()
+	}
+
+	if err != nil {
+		return nil, &ServiceError{u.Name(), serviceName, err}
+	}
+	lines, err := util.ReadLines(sout)
+	if len(lines) != 1 {
+		return nil, &ServiceError{u.Name(), serviceName, errors.New("Unexpected output: " + strings.Join(lines, "\n"))}
+	}
+
+	// Output will be "MainPID=1234" or
+	// "MainPID=0" if service does not exist.
+	line := lines[0]
+	fields := strings.Split(line, "=")
+	if fields[1] != "0" {
+		pid, err := strconv.ParseInt(fields[1], 10, 32)
+		if err != nil {
+			return nil, &ServiceError{u.Name(), serviceName, err}
+		}
+		return &ProcessStatus{int(pid), Up}, nil
+	}
+
+	if len(u.dummyOutput2) != 0 {
+		sout = []byte(u.dummyOutput2)
+	} else {
+		cmd := exec.Command("systemctl", "is-enabled", serviceName)
+		sout, err = cmd.CombinedOutput()
+	}
+	if err != nil || string(sout) != "enabled\n" {
+		return nil, &ServiceError{u.Name(), serviceName, ErrServiceNotFound}
+	} else {
+		return &ProcessStatus{0, Down}, nil
+	}
+}
---
+++ services/systemd_test.go
@@ -0,0 +1,38 @@
+package services
+
+import (
+	"github.com/stretchr/testify/assert"
+	"testing"
+)
+
+func TestDetectSystemd(t *testing.T) {
+	t.Parallel()
+	init, err := detectSystemd("etc/systemd")
+	assert.Nil(t, err)
+
+	upstart := init.(*Systemd)
+	upstart.dummyOutput = "MainPID=12345"
+	st, err := init.LookupService("mysql")
+	assert.Nil(t, err)
+	assert.Equal(t, 12345, st.Pid)
+	assert.Equal(t, Up, st.Status)
+
+	// bad name
+	upstart.dummyOutput = "MainPID=0"
+	upstart.dummyOutput2 = "Failed to issue method call: No such file or directory"
+	st, err = init.LookupService("foo")
+	assert.Nil(t, st)
+	assert.NotNil(t, err)
+
+	// process is down
+	upstart.dummyOutput = "MainPID=0"
+	upstart.dummyOutput2 = "enabled\n"
+	st, err = init.LookupService("memcached")
+	assert.NotNil(t, st)
+	assert.Nil(t, err)
+	assert.Equal(t, 0, st.Pid)
+	assert.Equal(t, Down, st.Status)
+
+	err = init.Restart("rsyslog")
+	assert.Nil(t, err)
+}
