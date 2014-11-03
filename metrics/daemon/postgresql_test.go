52b70ebc8a3edb98d4127ed4cb68162bf764e4e8
Mike Perham <mperham@gmail.com>
2014-11-03 15:24:03 -0800

    Fix tests

--- metrics/daemon/postgresql_test.go
+++ metrics/daemon/postgresql_test.go
@@ -23,17 +23,17 @@ func TestPostgresqlCollection(t *testing.T) {
 	data := metricMap{}
 	err := populate(rs, data, "deadlocks")
 	assert.Nil(t, err)
-	assert.Equal(t, metricMap{"deadlocks": 0, "rollbacks": 8.0, "blk_hit_rate": 0.9955268047622324}, data)
+	assert.Equal(t, metricMap{"blk_hit_rate": 99.55268047622324}, data)
 
 	rs.execFunk = testExec("fixtures/pg.sizeStats.output.txt")
 	err = populate(rs, data, "total_size")
 	assert.Nil(t, err)
-	assert.Equal(t, metricMap{"deadlocks": 0, "rollbacks": 8.0, "blk_hit_rate": 0.9955268047622324, "total_size": 122880.0}, data)
+	assert.Equal(t, metricMap{"blk_hit_rate": 99.55268047622324, "total_size": 122880.0}, data)
 
 	rs.execFunk = testExec("fixtures/pg.userStats.output.txt")
 	err = populate(rs, data, "seq_scans")
 	assert.Nil(t, err)
-	assert.Equal(t, metricMap{"deadlocks": 0, "rollbacks": 8.0, "blk_hit_rate": 0.9955268047622324, "total_size": 122880.0, "seq_scans": 6.0}, data)
+	assert.Equal(t, metricMap{"blk_hit_rate": 99.55268047622324, "total_size": 122880.0, "seq_scans": 6.0}, data)
 
 	err = populate(rs, data, "bad_metric")
 	assert.NotNil(t, err)
@@ -58,7 +58,7 @@ func TestRealPostgresqlConnection(t *testing.T) {
 
 	fmt.Printf("%v", hash)
 
-	assert.True(t, hash["rollbacks"] > 0, "This test will fail if you don't have postgresql installed")
+	assert.True(t, hash["blk_hit_rate"] > 0, "This test will fail if you don't have postgresql installed")
 }
 
 func psqlSource(metrics ...string) *pgSource {
@@ -67,7 +67,7 @@ func psqlSource(metrics ...string) *pgSource {
 		panic(err)
 	}
 	if len(metrics) == 0 {
-		metrics = []string{"deadlocks", "total_size", "seq_scans"}
+		metrics = []string{"blk_hit_rate", "total_size", "seq_scans"}
 	}
 	for _, x := range metrics {
 		src.Watch(x)
