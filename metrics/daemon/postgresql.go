cbde927af5247204f1b5e18caabba41041ac2fa5
Mike Perham <mperham@gmail.com>
2014-11-03 15:10:47 -0800

    Initial pass at some postgresql metrics, #26

---
+++ metrics/daemon/postgresql.go
@@ -0,0 +1,237 @@
+package daemon
+
+import (
+	"errors"
+	"fmt"
+	"strconv"
+	"strings"
+
+	"github.com/mperham/inspeqtor/util"
+)
+
+type pgSource struct {
+	Hostname    string
+	Port        string
+	Username    string
+	metrics     map[string]bool
+	captureRepl bool
+	execFunk    executor
+}
+
+func (pg *pgSource) Name() string {
+	return "postgresql"
+}
+
+func (pg *pgSource) Watch(metricName string) {
+	pg.metrics[metricName] = true
+}
+
+func (pg *pgSource) Capture() (metricMap, error) {
+	data := metricMap{}
+
+	for name, _ := range pg.metrics {
+		if _, ok := data[name]; !ok {
+			err := populate(pg, data, name)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	return data, nil
+}
+
+func (pg *pgSource) Prepare(funk executor) error {
+	_, err := runSql(pg, "select 1")
+	return err
+}
+
+func (pg *pgSource) ValidMetrics() []metric {
+	return pgMetrics
+}
+
+func (pg *pgSource) buildArgs() []string {
+	args := []string{"-Xt"}
+
+	if pg.Hostname != "" {
+		args = append(args, "-h")
+		args = append(args, pg.Hostname)
+	}
+	if pg.Port != "" {
+		args = append(args, "-p")
+		args = append(args, pg.Port)
+	}
+
+	if pg.Username != "" {
+		args = append(args, "-U")
+		args = append(args, pg.Username)
+	}
+	// Password must be specified via a ~/.pgpass file
+	// http://www.postgresql.org/docs/current/static/libpq-pgpass.html
+
+	return args
+}
+
+func buildPostgresqlSource(params map[string]string) (Collector, error) {
+	rs := &pgSource{"localhost", "5432", "root", map[string]bool{}, false, execCmd}
+	for k, v := range params {
+		switch k {
+		case "username":
+			rs.Username = v
+		case "hostname":
+			rs.Hostname = v
+		case "port":
+			_, err := strconv.ParseUint(v, 10, 32)
+			if err != nil {
+				return nil, err
+			}
+			rs.Port = v
+		}
+	}
+	return rs, nil
+}
+
+func populate(pg *pgSource, data metricMap, name string) error {
+	var sqlfunk func(*pgSource, metricMap) error
+	switch name {
+	case "rollbacks", "deadlocks", "blk_hit_rate":
+		sqlfunk = dbStats
+	case "seq_scans":
+		sqlfunk = userStats
+	case "total_size":
+		sqlfunk = sizeStats
+	default:
+		return errors.New("No such metric: " + name)
+	}
+	return sqlfunk(pg, data)
+}
+
+func userStats(pg *pgSource, data metricMap) error {
+	sql := "select sum(seq_scan) from pg_stat_user_tables"
+	results, err := runSql(pg, sql)
+	if err != nil {
+		return err
+	}
+	if len(results) != 1 {
+		return errors.New(fmt.Sprintf("Results size == %d", len(results)))
+	}
+	if len(results[0]) != 1 {
+		return errors.New(fmt.Sprintf("Results row size == %d", len(results[0])))
+	}
+
+	val := results[0][0]
+	if val == "" {
+		val = "0"
+	}
+	ival, err := strconv.ParseUint(val, 10, 64)
+	if err != nil {
+		return err
+	}
+	data["seq_scans"] = float64(ival)
+	return nil
+}
+
+func sizeStats(pg *pgSource, data metricMap) error {
+	sql := `select sum(pg_total_relation_size(pg_class.oid))
+					FROM pg_class LEFT JOIN pg_namespace N ON (N.oid = pg_class.relnamespace)
+					WHERE nspname NOT IN ('pg_catalog', 'information_schema') AND
+								nspname !~ '^pg_toast' AND relkind IN ('r')`
+	results, err := runSql(pg, sql)
+	if err != nil {
+		return err
+	}
+	if len(results) != 1 {
+		return errors.New(fmt.Sprintf("Results size == %d", len(results)))
+	}
+	if len(results[0]) != 1 {
+		return errors.New(fmt.Sprintf("Results row size == %d", len(results[0])))
+	}
+
+	val := results[0][0]
+	if val == "" {
+		val = "0"
+	}
+	ival, err := strconv.ParseUint(val, 10, 64)
+	if err != nil {
+		return err
+	}
+	data["total_size"] = float64(ival)
+	return nil
+}
+
+func dbStats(pg *pgSource, data metricMap) error {
+	sql := "select sum(xact_rollback), sum(deadlocks), sum(blks_hit) / (sum(blks_read) + sum(blks_hit)) from pg_stat_database"
+	results, err := runSql(pg, sql)
+	if err != nil {
+		return err
+	}
+	if len(results) != 1 {
+		return errors.New(fmt.Sprintf("Results size == %d", len(results)))
+	}
+	if len(results[0]) != 3 {
+		return errors.New(fmt.Sprintf("Results row size == %d", len(results[0])))
+	}
+
+	val := results[0][0]
+	ival, err := strconv.ParseUint(val, 10, 64)
+	if err != nil {
+		return err
+	}
+	data["rollbacks"] = float64(ival)
+
+	val = results[0][1]
+	ival, err = strconv.ParseUint(val, 10, 64)
+	if err != nil {
+		return err
+	}
+	data["deadlocks"] = float64(ival)
+
+	val = results[0][2]
+	fval, err := strconv.ParseFloat(val, 64)
+	if err != nil {
+		return err
+	}
+	data["blk_hit_rate"] = fval
+	return nil
+}
+
+func runSql(pg *pgSource, stmt string) ([][]string, error) {
+	args := pg.buildArgs()
+	args = append(args, "-c")
+	args = append(args, stmt)
+	sout, err := pg.execFunk("psql", args, nil)
+	if err != nil {
+		return nil, errors.New(string(sout))
+	}
+
+	lines, err := util.ReadLines(sout)
+	if err != nil {
+		return nil, err
+	}
+
+	var table [][]string
+
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		var row []string
+		cells := strings.Split(line, "|")
+		for _, cell := range cells {
+			row = append(row, strings.TrimSpace(cell))
+		}
+		table = append(table, row)
+	}
+
+	return table, nil
+}
+
+var (
+	pgMetrics []metric = []metric{
+		metric{"rollbacks", c, nil},
+		metric{"deadlocks", c, nil},
+		metric{"blk_hit_rate", g, nil},
+		metric{"seq_scans", c, nil},
+		metric{"total_size", c, &funcWrapper{inMB, nil}},
+	}
+)
---
+++ metrics/daemon/postgresql_test.go
@@ -0,0 +1,76 @@
+package daemon
+
+import (
+	"fmt"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestBadPostgresqlConfig(t *testing.T) {
+	t.Parallel()
+	src, err := Sources["postgresql"](map[string]string{"port": "885u"})
+	assert.Nil(t, src)
+	assert.NotNil(t, err)
+}
+
+func TestPostgresqlCollection(t *testing.T) {
+	t.Parallel()
+	rs := psqlSource()
+	assert.NotNil(t, rs)
+
+	rs.execFunk = testExec("fixtures/pg.dbStats.output.txt")
+	data := metricMap{}
+	err := populate(rs, data, "deadlocks")
+	assert.Nil(t, err)
+	assert.Equal(t, metricMap{"deadlocks": 0, "rollbacks": 8.0, "blk_hit_rate": 0.9955268047622324}, data)
+
+	rs.execFunk = testExec("fixtures/pg.sizeStats.output.txt")
+	err = populate(rs, data, "total_size")
+	assert.Nil(t, err)
+	assert.Equal(t, metricMap{"deadlocks": 0, "rollbacks": 8.0, "blk_hit_rate": 0.9955268047622324, "total_size": 122880.0}, data)
+
+	rs.execFunk = testExec("fixtures/pg.userStats.output.txt")
+	err = populate(rs, data, "seq_scans")
+	assert.Nil(t, err)
+	assert.Equal(t, metricMap{"deadlocks": 0, "rollbacks": 8.0, "blk_hit_rate": 0.9955268047622324, "total_size": 122880.0, "seq_scans": 6.0}, data)
+
+	err = populate(rs, data, "bad_metric")
+	assert.NotNil(t, err)
+}
+
+func TestRealPostgresqlConnection(t *testing.T) {
+	t.Parallel()
+
+	/*
+		To get this test running locally, you must have a root superuser:
+		  createuser -s root
+			createdb root
+	*/
+
+	rs := psqlSource()
+	assert.NotNil(t, rs)
+	err := rs.Prepare(execCmd)
+	assert.Nil(t, err)
+	hash, err := rs.Capture()
+	assert.Nil(t, err)
+	assert.NotNil(t, hash)
+
+	fmt.Printf("%v", hash)
+
+	assert.True(t, hash["rollbacks"] > 0, "This test will fail if you don't have postgresql installed")
+}
+
+func psqlSource(metrics ...string) *pgSource {
+	src, err := Sources["postgresql"](map[string]string{})
+	if err != nil {
+		panic(err)
+	}
+	if len(metrics) == 0 {
+		metrics = []string{"deadlocks", "total_size", "seq_scans"}
+	}
+	for _, x := range metrics {
+		src.Watch(x)
+	}
+	return src.(*pgSource)
+}
